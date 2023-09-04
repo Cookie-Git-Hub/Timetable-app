e4ae8cb21895b9c68dd686bcc7ee285e7df4526a
Cookie <foofoof1234567890@gmail.com>
2023-09-04 21:43:17 +0200

    Парсинг

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 
+def parsing():
+    url = 'http://bseu.by/schedule/'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        page_content = response.text
+        soup = BeautifulSoup(page_content, 'html.parser')
+        div_content = soup.find_all('tbody')
+        for content_div in div_content:
+            print(content_div.text)
+    else:
+        print('ERROR')
 
-class MyApp(App):
+parsing()
 
-    def build(self):
-        return Label(text='Hello world')
+# class MyApp(App):
+
+    
+
+#      def build(self):
+#          return Label(text='Hello world')
 
 
-if __name__ == '__main__':
-    MyApp().run()
+# if __name__ == '__main__':
+#     MyApp().run()
---
+++ parsing.py
@@ -0,0 +1,42 @@
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+driver = wd.Chrome()
+
+# Открываем страницу сайта
+driver.get("http://bseu.by/schedule/")
+
+# Выбираем факультет и курс (пример)
+faculty_select = Select(driver.find_element(By.ID, "faculty"))
+faculty_select.select_by_visible_text("ФЦЭ")
+time.sleep(1)
+
+form_select = Select(driver.find_element(By.ID, "form"))
+form_select.select_by_visible_text("Дневная")
+time.sleep(1)
+
+course_select = Select(driver.find_element(By.ID, "course"))
+course_select.select_by_visible_text("1")
+time.sleep(1)
+
+group_select = Select(driver.find_element(By.ID, "group"))
+group_select.select_by_visible_text("23 ДЦИ-1 | Экономическая информатика")
+time.sleep(1)
+
+select_period = driver.find_element(By.ID, "pweek")
+select_period.click()
+time.sleep(1)
+
+btn_click = driver.find_element(By.ID, "btn")
+btn_click.click()
+
+time.sleep(3)
+
+# Теперь вы можете извлечь информацию из расписания, например, с помощью XPath или CSS-селекторов
+schedule_elements = driver.find_elements(By.XPATH, "//div[@id='content']")
+for schedule_element in schedule_elements:
+    print(schedule_element.text)
+
+driver.quit()
