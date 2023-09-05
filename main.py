64fdf0fad21cf06b3fc148cf24be140f58afa243
Cookie <foofoof1234567890@gmail.com>
2023-09-05 17:09:27 +0200

    321

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from parsing import timetable_parsing
+
+#Получаем токен из защищённого файла
+with open('config.env', 'r') as file:
+    token = file.readline()
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="6638335742:AAEeugDQNawltqGfVIYEzeomQ4HkbPLpoO8")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    text = timetable_parsing()
+    await message.answer(text)
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- parsing.py
+++ parsing.py
@@ -4,39 +4,39 @@ from selenium.webdriver.support.ui import Select
 import time
 
 driver = wd.Chrome()
-
 # Открываем страницу сайта
 driver.get("http://bseu.by/schedule/")
 
-# Выбираем факультет и курс (пример)
-faculty_select = Select(driver.find_element(By.ID, "faculty"))
-faculty_select.select_by_visible_text("ФЦЭ")
-time.sleep(0.02)
+def timetable_parsing():
+    faculty_select = Select(driver.find_element(By.ID, "faculty"))
+    faculty_select.select_by_visible_text("ФЦЭ")
+    time.sleep(0.02)
 
-form_select = Select(driver.find_element(By.ID, "form"))
-form_select.select_by_visible_text("Дневная")
-time.sleep(0.02)
+    form_select = Select(driver.find_element(By.ID, "form"))
+    form_select.select_by_visible_text("Дневная")
+    time.sleep(0.02)
 
-course_select = Select(driver.find_element(By.ID, "course"))
-course_select.select_by_visible_text("1")
-time.sleep(0.02)
+    course_select = Select(driver.find_element(By.ID, "course"))
+    course_select.select_by_visible_text("1")
+    time.sleep(0.02)
 
-group_select = Select(driver.find_element(By.ID, "group"))
-group_select.select_by_visible_text("23 ДЦИ-1 | Экономическая информатика")
-time.sleep(0.02)
+    group_select = Select(driver.find_element(By.ID, "group"))
+    group_select.select_by_visible_text("23 ДЦИ-1 | Экономическая информатика")
+    time.sleep(0.02)
 
-select_period = driver.find_element(By.ID, "pweek")
-select_period.click()
-time.sleep(0.02)
+    select_period = driver.find_element(By.ID, "pweek")
+    select_period.click()
+    time.sleep(0.02)
 
-btn_click = driver.find_element(By.ID, "btn")
-btn_click.click()
+    btn_click = driver.find_element(By.ID, "btn")
+    btn_click.click()
 
-time.sleep(0.1)
+    time.sleep(0.1)
 
-# Теперь вы можете извлечь информацию из расписания, например, с помощью XPath или CSS-селекторов
-schedule_elements = driver.find_elements(By.XPATH, "//div[@id='content']")
-for schedule_element in schedule_elements:
-    print(schedule_element.text)
+    # Теперь вы можете извлечь информацию из расписания, например, с помощью XPath или CSS-селекторов
+    schedule_elements = driver.find_elements(By.XPATH, "//div[@id='content']")
+    for schedule_element in schedule_elements:
+        print(schedule_element.text)
 
-driver.quit()
+    driver.quit()
+    return schedule_elements
